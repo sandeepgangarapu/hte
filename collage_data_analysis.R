1e07271abaaaca9cb6e5c2cdcd919da089dd9faa
Sandeep Kumar Gangarapu <sandeepgangarapu.iitkgp@gmail.com>
2017-02-21 17:36:16 -0600

    More changes

--- collage_data_analysis.R
+++ collage_data_analysis.R
@@ -20,7 +20,15 @@ for(i in 1:nrow(data)) {
 sqldf('select treatment_group, sum(number_referrals), count(treatment_group),  
       sum(number_referrals)/count(treatment_group) from data group by treatment_group')
 
-#subsetting data to contain just 'altruistic group' so as to do heterogeneity
-altruistic_data <- sqldf('select * from data where cell = 4')
+#average effect of all groups
+avg_effect <- data %>% group_by(treatment_group) %>% summarise(avg = mean(number_referrals))
 
+#average treatment effect of altruistic group
+avg_effect[avg_effect$treatment_group=='altruistic','avg'] - avg_effect[avg_effect$treatment_group=='control','avg']
 
+#subsetting data to contain just 'altruistic group and control group' so as to do heterogeneity
+altruistic_data <- data %>% filter(cell==c(4,1))
+
+#Subsetting variables that matter
+alt_data_filter <- altruistic_data[,c("satisfied","NPS","lastday_purchase_all","num_purchase_all",
+                                      "money_spend_all","survey", "number_referrals")]
